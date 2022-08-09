4c5edb7967cd43148363a294a9ddb68e674626b9
Akashrajeev1 <110896835+Akashrajeev1@users.noreply.github.com>
2022-08-09 15:24:29 +0530

    Add files via upload

---
+++ advanced_list.py
@@ -0,0 +1,54 @@
+# first=[1,2,3,4,5]
+# second=[6,7,8,9,10]
+# third=first+second
+# print(third)
+# first.extend(second)
+# print(first)
+# print(second)
+#
+#
+#
+# print("--------------------------------------------")
+#
+#
+# remove_list=[1,2,3,4,5,"hello"]
+# print(remove_list)
+# remove_list.remove(1)
+# print(remove_list)
+# remove_list.remove(2)
+# print(remove_list)
+# remove_list.pop(0)
+# print(remove_list)
+# remove_list.pop()
+# print(remove_list)
+# del remove_list[0]
+# print(remove_list)
+# del remove_list
+# print(remove_list)
+#
+# print("--------------------------------------------------------")
+
+
+# for_list=[1,2,3,4,5,6,7,8,9,10]
+# for x in for_list:
+#     print(x)
+#     print(x*x)
+#     print(x*x*x)
+#     print(x*x*x*x)
+
+# listt=[["thanha",23,"place","luminar"],["naja",23,"place","luminar"],["aswin",23,"place","luminar"]]
+# for i in listt:
+#     print(i[0],"is a good student")
+#     print(i[1],"is the age")
+#     print(i[2],"is the place")
+#     print(i[3],"is the institute of student")
+#     print("\n")
+#     print("next student")
+
+new=["thanha",23,"place","luminar"]
+for i in range(0,len(new)):
+    print(new[i])
+print("----------------------------------------------")
+print(len(new))
+sum=range(0,len(new))
+print(len(sum))
